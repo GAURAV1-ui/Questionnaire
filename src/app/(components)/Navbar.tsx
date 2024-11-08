41c72d34614f7c8366ee0e6694af09773c202b26
Gaurav <iamgaurav201@gmail.com>
2024-11-08 22:42:04 +0530

    Final commit

--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -1,9 +1,12 @@
 
 import React from "react";
+import Image from "next/image";
+import Logo from "../../assets/Kreatoors Logo.svg"
 
 const Navbar = () => {
   return (
     <header className="flex justify-left p-4 font-poppins md:px-8">
+      <Image src={Logo} alt="Kreatoors Logo" className="w-8 h-8"/>
       <h1 className="text-xl font-bold text-black">Kreatoors</h1>
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-// Home.tsx
 import Head from "next/head";
 import Questionnaire from "./question/page";
 import Navbar from "./(components)/Navbar";
 
 export default function Home() {
   return (
-    <div className="md: ml-10">
+    <div className="md:ml-10">
       <Head>
-        <title>AILovesHR</title>
+        <title>Kreatoors</title>
       </Head>
       <Navbar />
       <main className="flex items-center  flex-grow">
